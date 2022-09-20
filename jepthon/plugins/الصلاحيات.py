9330a64c7b510d0f62e792c8d585ef3054f0a524
moaziza <85038472+moaziza@users.noreply.github.com>
2022-09-20 18:52:37 +0200

    Add files via upload

--- jepthon/plugins/الصلاحيات.py
+++ jepthon/plugins/الصلاحيات.py
@@ -17,8 +17,7 @@ from . import BOTLOG, get_user_from_event
 
 plugin_category = "admin" 
 
-# Copyright (C) 2021 JepThon TEAM
-# FILES WRITTEN BY  @lMl10l
+
 
 @jepiq.ar_cmd(
     pattern="قفل (.*)",
